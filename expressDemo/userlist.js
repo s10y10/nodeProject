cd643bbf8e61a5d44d37e723d205d46209c8f83b
s10y10 <248496385@qq.com>
2017-05-24 10:37:21 +0800

    userlist

--- expressDemo/userlist.js
+++ expressDemo/userlist.js
@@ -5,16 +5,35 @@ var express = require("express");
 var app = express();
 var fs = require("fs");
 
+var user = {
+    "name":"user4",
+    "pwd":"pwd4",
+    "id":4
+};
+
 app.get("/getUserList",function(req,res){
     fs.readFile(__dirname + "/" + "userdata.json","utf8",function(err,data)
     {
-        console.log(data);
         res.end(data);
     });
 });
 
-app.get("/delUser/:id",function(req,res){
+app.get("/addUser",function(req,res){
+   fs.readFile(__dirname + "/" + "userdata.json","utf8",function(err,data)
+   {
+      var obj = JSON.parse(data);
+       console.log(obj);
+       obj["user4"] = user;
+       res.end(JSON.stringify(obj));
+   });
+});
+
+app.get("/delUser/:id   ",function(req,res){
     fs.readFile(__dirname + "/" + "userdata.json", "utf8",function(err,data){
-        
+        var obj = JSON.parse(data);
+        console.log(req.params);
+        delete obj["user"+req.params.id];
+        res.end(JSON.stringify(obj));
     });
 });
+app.listen(8888);
